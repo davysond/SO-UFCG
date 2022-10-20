dd21f849f35f51faa77cce78c21aefb082e5ea28
Davyson D <davysonguimares74@gmail.com>
2022-10-20 19:32:27 -0300

    Adicionando algoritmo para o problema clássico do Produtor e Consumidor, sem uso de semáforos ou quaisquer outros mecanismos que façam exclusão mútua.

---
+++ Algorithms/MyProducerConsumer.py
@@ -0,0 +1,46 @@
+from random import randint
+from time import sleep
+
+buffer = []  #Lista para armazenamento dos valores gerados;
+max_size = 5  #Tamanho máximo do buffer;
+inative_c = False  #Flag booleana para ativar/desativar o processo consumidor;
+inative_p = False  #Flag booleana para ativar/desativar o processo produtor.
+
+
+def producer():  #Função que cria um produtor;
+  global buffer, max_size, inative_c, inative_p
+  if not inative_p:  #Verifica se o processo do produtor está ativo;
+    while len(buffer) < max_size:  #Verifica se o buffer não está cheio;
+      buffer.append(
+        randint(0, 5)
+      )  #Caso não esteja, adiciona um valor gerado no intervalo especificado;
+      print("O Produtor está produzindo:", buffer)
+    else:
+      print("O Buffer está cheio:", buffer)
+      print(
+        "O Produtor ficará inativo até que o buffer esteja com algum espaço vazio..."
+      )
+      inative_p = True  #Desativa o processo do produtor.
+      inative_c = False  #Ativa o processo do consumidor.
+      consumer()  #Chama o consumidor.
+
+
+def consumer():  #Função que cria um consumidor;
+  global buffer, max_size, inative_c, inative_p
+  if not inative_c:  #Verifica se o processo do produtor está ativo;
+    if len(buffer) > 0:  #Verifica se a lista não está vazia;
+      del buffer[0]  #Remove o primeiro elemento da lista.
+      print("O Consumidor está consumindo:", buffer)
+    else:
+      print("O Buffer está vazio:", buffer)
+      print(
+        "O Consumidor ficará inativo até que haja algum produto no buffer...")
+      inative_c = True
+      inative_p = False
+      producer()
+
+
+while True:
+  producer()
+  consumer()
+  sleep(1)
